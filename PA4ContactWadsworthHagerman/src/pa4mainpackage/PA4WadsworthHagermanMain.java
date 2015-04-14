26e5dbf5d42e9717dc2fa3fdb14954fbc2a6440e
Ben Wadsworth <Ben Wadsworth@141.139.90.45>
2015-04-14 16:33:12 -0400

    Added main driver class and set up the basic package hierarchy (which is subject to change).
    John - If you're planning on doing the GUI then make sure it is separated into a view and controller class.

---
+++ PA4ContactWadsworthHagerman/src/pa4mainpackage/PA4WadsworthHagermanMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pa4mainpackage;
+
+/**
+ *
+ * @author Ben Wadsworth
+ */
+public class PA4WadsworthHagermanMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is test code.");
+    }
+    
+}
