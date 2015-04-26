b223bb4d3fd93cde0e99548aa6ab580626f7f036
JohnThomas <JohnThomas@141.139.246.250>
2015-04-26 03:18:29 -0400

    WORKING ON PRINTIN

--- PA4ContactWadsworthHagerman/src/pa4mainpackage/model/ModelHandler.java
+++ PA4ContactWadsworthHagerman/src/pa4mainpackage/model/ModelHandler.java
@@ -1,10 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pa4mainpackage.model;
 
+package pa4mainpackage.model;
 
 
 import java.util.ArrayList;
@@ -14,19 +9,31 @@ import java.util.*;
 
 /**
  * MAIN CLASS TO INTERACT WITH SUB-MODEL CLASSES!
- * @author Ben Wadsworth
+ * @author Ben Wadsworth, John Hagerman
  */
 public class ModelHandler 
 {
     TreeMap storage;
     PrimaryKeyHandler keys;
     
+    /**
+     *
+     */
     public ModelHandler()
     {
         storage = new TreeMap();
         keys = new PrimaryKeyHandler();
-    }
+        
+    }//ENd of Constructor
     
+    /**
+     *
+     * @param firstName
+     * @param lastName
+     * @param organization
+     * @param phoneNumbers
+     * @param emailAddresses
+     */
     public void addContact(String firstName, String lastName,String organization, 
             ArrayList<String> phoneNumbers,
             ArrayList<String>emailAddresses)
@@ -34,8 +41,13 @@ public class ModelHandler
         Contact tempContact;
         tempContact = new Contact(keys.getNextAvailableKey(), firstName, lastName, organization, phoneNumbers, emailAddresses);
         storage.put(tempContact.getKeyID(), tempContact);
-    }
+        
+    }//End of addContact() Method
     
+    /**
+     *
+     * @param treeLocation
+     */
     public void deleteContact(Integer treeLocation)
     {
         if(storage.containsKey(treeLocation))
@@ -43,37 +55,123 @@ public class ModelHandler
             storage.remove(treeLocation);
             keys.addAKey(treeLocation);
         }
-    }
+        
+    }//End of deleteContact() method
     
+    /**
+     *
+     * @param treeLocation
+     * @return
+     */
     public Contact getContact(Integer treeLocation)
     {
         return((Contact) storage.get(treeLocation));
-    }
+        
+    }//End of getContact(Integer treeLocation) method
     
+    /**
+     *
+     * @param treeLocation
+     * @return
+     */
     public String formattedGUIContact(Integer treeLocation)
     {
         String returnString = "";
         Contact tempContact = (Contact) storage.get(treeLocation);
         
-        returnString = 
-           //     "ID    -     Last Name     -     First Name     -     Organization     -     " +
-           //     "Phone Numbers   -   Email Addresses\n" + 
-           //    "====================================================================" +
-            //    "============================\n" +
-             //   treeLocation + "     " + tempContact.getLastName() + String.format("%" + (29 - (tempContact.getLastName().length() * 2)) + "s", " ") + 
-             //   tempContact.getFirstName() + String.format("%" + (30 - tempContact.getFirstName().length() * 2) + "s", " ") + 
-             //   tempContact.getOrganization() + String.format("%" + (19 - tempContact.getOrganization().length()) + "s", " ") +
-             //   tempContact.getContactPhoneNumber().getAllPhoneNumbers();
-                
+        String phoneString = tempContact.getContactPhoneNumber().getAllPhoneNumbers();
+        List<String> phoneList = new ArrayList<>(Arrays.asList(phoneString.split(" ")));
+        String phoneListString = "           ";
+        String emailString = tempContact.getContactEmailAddress().getAllEmailAddresses();
+        List<String> emailList = new ArrayList<>(Arrays.asList(emailString.split(" ")));
+        String emailListString = "           ";
         
+        String extraString = "";
+        
+        String[][] outPutArray = new String[6][2];
+        
+        if(emailList.isEmpty() == false)
+            emailListString = emailList.get(0);
+        
+        if(phoneList.isEmpty() == false)
+            phoneListString = phoneList.get(0);
+
+        
+        for (int i = 0; i < phoneList.size(); i++)
+        {
+            outPutArray[i][0] = phoneList.get(i);		
+        }
+        for (int i = 0; i < emailList.size(); i++)
+        {
+            outPutArray[i][1] = emailList.get(i);
+        }
+        
+        if((outPutArray[1][0] != null) || (outPutArray[1][1] != null))
+        {
+            extraString +="\n";
+        }
+       
+        for(int i = 1; i < 5; i++)
+        {
+            if( (outPutArray[i][0] != null) && (outPutArray[i][1] != null)) 
+            {
+                extraString += String.format("%71s", outPutArray[i][0]) + " "
+                        + outPutArray[i][1]; 
+                  
+                  if(i != 5)
+                      if((outPutArray[i + 1][0] != null) && (outPutArray[i + 1][1] != null));
+                          extraString += "\n";                    
+            }
+     
+            else if( (outPutArray[i][0] == null) && (outPutArray[i][1] != null)) 
+            {
+                extraString += String.format("%71s", "           ") + " "
+                        + outPutArray[i][1]; 
+                
+                if(i != 5)
+                    if((outPutArray[i + 1][0] != null) && (outPutArray[i + 1][1] != null));
+                        extraString += "\n";
+
+                
+            }
+            else if( (outPutArray[i][0] != null) && (outPutArray[i][1] == null)) 
+            {
+                 extraString += String.format("%71s", outPutArray[i][0]);
+                 
+                 if(i != 5)
+                    if((outPutArray[i + 1][0] != null) && (outPutArray[i + 1][1] != null));
+                        extraString += "\n";
+         
+            }             
+        }
+       
+        returnString = "  " +
+                String.format("%-8s", treeLocation) + " " +
+                String.format("%-16s",tempContact.getLastName()) + " " +
+                String.format("%-16s",tempContact.getFirstName()) + " " +
+                String.format("%-16s",tempContact.getOrganization()) +
+                phoneListString + " " + emailListString + extraString ;
+                         
         return(returnString);
-    }
+        
+    }//End of formattedGUIContact(Integer treeLocation) method
     
+    /**
+     *
+     * @return String returnString
+     */
     public TreeMap getTreeMapStorage()
     {
         return(storage);
-    }
+        
+    }//End of getTreeMapStorage() method
     
+    
+    /**
+     *  
+     * @return String returnSting
+     */
+    @Override
     public String toString()
     {
          Set set = storage.entrySet();
@@ -90,5 +188,7 @@ public class ModelHandler
          }
         
          return(returnString);
-    }
-}
+         
+    }//End of toString() method
+    
+}//End of ModelHandler Class
