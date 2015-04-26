29ee6da7b7f159c9f1387de7c951302a0f29afa5
JohnThomas <JohnThomas@141.139.246.250>
2015-04-26 00:19:03 -0400

    WORKING ON PRINTIN

--- PA4ContactWadsworthHagerman/src/pa4mainpackage/delegate/MainView.java
+++ PA4ContactWadsworthHagerman/src/pa4mainpackage/delegate/MainView.java
@@ -3,12 +3,16 @@ package pa4mainpackage.delegate;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.Set;
 import java.util.TreeMap;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -35,26 +39,31 @@ public class MainView
                       menuEditDelete, menuEditAdd, menuSortFindMax, menuSortFindMin, menuFileOpen,
                       menuEditEditEntry;
     private JFrame mainWindow, aboutFrame, addFrame;
-    private JPanel panel1, north, south, tableAreaPanel;
+    private JPanel panel1, north, south, tableAreaPanel, test;
     private JMenu menuFile, menuSort, menuAbout, menuEdit;
     private JButton closeAboutButton;
     private ActionListener viewAl;
     private JScrollPane scrolledText;
+    private JTextArea textAreaText;
+    private JLabel piss, piss2;
     private JTextField nameField, lastNameField, organizationField;
-    private static final int LINES = 21;
-    private static final int CHAR_PER_LINE = 65;
+    //private static final int LINES = 21;
+   // private static final int CHAR_PER_LINE = 15;
+    private JScrollPane scrollpane;
 
 
     public MainView()
     {
-        mainMenu();            
+        
+        mainMenu();       
+        
     }
     
     private void mainMenu()
     {
         mainWindow = new JFrame();
         mainWindow.setTitle("Assignment IV: Directory");
-        mainWindow.setSize( 800, 400);
+        mainWindow.setSize( 850, 400);
         mainWindow.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
         mainWindow.setResizable(true);
         mainWindow.setLayout( new BorderLayout());
@@ -145,11 +154,36 @@ public class MainView
         //creating text area
         tableAreaPanel = new JPanel();
         tableAreaPanel.setLayout(new BorderLayout());
-        //textPanel.setBackground(Color.DARK_GRAY);
+        tableAreaPanel.setLayout(new BoxLayout(tableAreaPanel,BoxLayout.PAGE_AXIS));
+        
+        JLabel keyLabel = new JLabel("ID    -     Last Name     -     First Name     -     Organization     -     "  +
+                 "Phone Numbers   -   Email Addresses\n");
+        
+        Font font2 = new Font("Courier New", Font.PLAIN, 12);
+        keyLabel.setFont(font2);
+         
+        tableAreaPanel.add(keyLabel);
+        
+        
+        //JPanel textPanel = new JPanel();
+        
+       // JScrollPane scrollPane = new JScrollPane();
+       // scrollpane = new JScrollPane();
+    
+       // setPreferredSize(new Dimension(450, 110));
+     
+       // mainWindow.add(scrollpane, BorderLayout.CENTER);
+
+        
+
+//textPanel.setBackground(Color.DARK_GRAY);
         //textAreaText = new JTextArea( LINES, CHAR_PER_LINE);
-        //textAreaText.setBackground(Color.WHITE);
-        //scrolledText = new JScrollPane(textAreaText);
-        //textPanel.add(scrolledText);
+        textAreaText = new JTextArea( );
+        textAreaText.setBackground(Color.WHITE);
+        Font font = new Font("Courier New", Font.PLAIN, 12);
+        textAreaText.setFont(font );
+        scrolledText = new JScrollPane(textAreaText);
+        tableAreaPanel.add(scrolledText);
         mainWindow.add(tableAreaPanel, BorderLayout.CENTER);
         
 
@@ -207,36 +241,47 @@ public class MainView
         }
     }
     
-//    public void updateContactViewPanel(TreeMap contactTreeMap)
-//    {
-//        Set<Integer> keys = contactTreeMap.keySet();
-//        JScrollPane contactContainer = new JScrollPane();
-//        
-//        for(Integer key : keys)
-//        {
-//            JPanel tempPanel = new JPanel();
-//            tempPanel.add(new JLabel("Tits"));
-//            contactContainer.add(tempPanel);
-//        }
-//        
-//        tableAreaPanel.add(contactContainer, BorderLayout.CENTER);
-//        
-//        mainWindow.getContentPane().add(tableAreaPanel);
-//
-//        mainWindow.pack();
-//    }
+   // public void updateContactViewPanel(TreeMap contactTreeMap)
+    //public void updateContactViewPanel()
+   // {
+
+        
+        
+    //    Set<Integer> keys = contactTreeMap.keySet();
+    //    JScrollPane contactContainer = new JScrollPane();
+    //   
+    //    for(Integer key : keys)
+    //   {
+     //      JPanel tempPanel = new JPanel();
+    //       tempPanel.add(new JLabel("Tits"));
+      //     contactContainer.add(tempPanel);
+     //  }
+      
+      //tableAreaPanel.add(contactContainer, BorderLayout.CENTER);
+      
+     //  mainWindow.getContentPane().add(tableAreaPanel);
+     // mainWindow.pack(); 
+   // }
     
-    public void updateTableArea(JTable table)
-    {
-        tableAreaPanel.removeAll();
-        JScrollPane spTable = new JScrollPane(table);
-        spTable.setSize(tableAreaPanel.getWidth(), tableAreaPanel.getHeight());
-        tableAreaPanel.add(spTable, BorderLayout.CENTER);
+    
+   // public void updateTableArea(JTable table)
+   // {
+   //     tableAreaPanel.removeAll();
+   //     JScrollPane spTable = new JScrollPane(table);
+   //     spTable.setSize(tableAreaPanel.getWidth(), tableAreaPanel.getHeight());
+    //    tableAreaPanel.add(spTable, BorderLayout.CENTER);
         
        // mainWindow.getContentPane().removeAll();
-        mainWindow.getContentPane().add(tableAreaPanel);
+    //    mainWindow.getContentPane().add(tableAreaPanel);
 
-        mainWindow.pack();
+    ///    mainWindow.pack();
+    //}
+    
+    
+    public void updateTextBox(String x)
+    {
+        textAreaText.removeAll();
+        textAreaText.setText(x);
     }
     
     public void setActionListeners(ActionListener al)
